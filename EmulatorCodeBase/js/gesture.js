8b008949551888e8150b9e34721b0e03a8c63111
Noriko <norko.misra@ischool.berkeley.edu>
2014-12-11 13:40:09 -0800

    image uv0

--- EmulatorCodeBase/js/gesture.js
+++ EmulatorCodeBase/js/gesture.js
@@ -18,6 +18,7 @@ define({
         'use strict';
         console.log('gesture::def');
         
+<<<<<<< Updated upstream
         var e = req.core.event;
 
         function init() {
@@ -70,6 +71,33 @@ define({
                 	tau.changePage('#WarningSunburn');
                 }
             });   
+=======
+    	console.log("Gesture: swipt right");
+    	
+    	if (pageid === 'Apply') {
+            tau.changePage('#Risk');
+        } else if (pageid === 'Risk') {
+            tau.changePage('#Current');
+        } else if (pageid === 'Current') {
+            tau.changePage('#Detecting');
+            //Temp: No UV detecting
+            setTimeout(function(){tau.changePage('#Current');}, 4000);
+        }
+    });
+    
+    
+    // Temp: for demo, show warning message by swipe up
+    var hammertime = Hammer(element).on("swipeup", function(event) {
+    	page = document.getElementsByClassName('ui-page-active')[0];
+        pageid = page ? page.id : '';	
+        
+    	console.log("Gesture: swipt up");
+    	
+    	if (pageid === 'Risk') {
+            tau.changePage('#WarningSunscreenExpired');
+        } else if (pageid === 'WarningSunscreenExpired'){
+        	tau.changePage('#WarningSunburn');
+>>>>>>> Stashed changes
         }
 
         return {
